ab38016efe738bc9fb3443246d101b1d19b506d3
DrackDMG <drackmg@gmail.com>
2025-09-30 17:18:24 -0600

    platform_id server o cliente

--- src/app/pages/pricing/pricing-pages.component.ts
+++ src/app/pages/pricing/pricing-pages.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
@@ -13,10 +14,19 @@ export default class PricingPagesComponent implements OnInit {
   private title = inject(Title);
   private meta= inject(Meta);
 
+  private platform = inject(PLATFORM_ID);
+
   ngOnInit(): void {
-    this.title.setTitle('Pricing');
-    this.meta.updateTag({ name: 'description', content: 'Pricing' });
-    this.meta.updateTag({ name: 'og:title', content: 'Pricing' });
-    this.meta.updateTag({ name: 'keywords', content: 'Pricing' });
+    //document.title = 'Pricing';
+    // console.log(this.platform);
+    // console.log(isPlatformBrowser(this.platform));
+    // console.log(isPlatformServer(this.platform));
+    // if (isPlatformBrowser(this.platform)) {
+    //   document.title = 'Pricing';//este ya no da error pero no se actualiza
+    // }
+     this.title.setTitle('Pricing'); //este es el que funciona
+     this.meta.updateTag({ name: 'description', content: 'Pricing' });
+     this.meta.updateTag({ name: 'og:title', content: 'Pricing' });
+     this.meta.updateTag({ name: 'keywords', content: 'Pricing' });
   }
 }
