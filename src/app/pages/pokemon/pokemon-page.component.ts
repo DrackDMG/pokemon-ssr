33a41fad4da9ea25ffa3cda3d3785ce10bc56459
DrackDMG <drackmg@gmail.com>
2025-10-01 17:15:30 -0600

    skeleton

--- src/app/pages/pokemon/pokemon-page.component.ts
+++ src/app/pages/pokemon/pokemon-page.component.ts
@@ -1,11 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit, signal } from '@angular/core';
 import { PokemonListComponent } from "../../pokemon/components/pokemon-list/pokemon-list.component";
+import { PokemonListSkeletonComponent } from "./ui/pokemon-list-skeleton/pokemon-list-skeleton.component";
 
 @Component({
   selector: 'app-pokemon-page',
-  imports: [PokemonListComponent],
+  imports: [PokemonListComponent, PokemonListSkeletonComponent],
   templateUrl: './pokemon-page.component.html',
   styleUrl: './pokemon-page.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PokemonPageComponent { }
+export default class PokemonPageComponent implements OnInit {
+
+  public isLoading = signal(true);
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.isLoading.set(false);
+    }, 3000);
+  }
+}
