0d29cc7a47934205ab15266c6f3c750af8cc9555
DrackDMG <drackmg@gmail.com>
2025-10-02 17:37:20 -0600

    pruebas AppComponent

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,25 +1,31 @@
-import { provideZonelessChangeDetection } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
+import { Component, provideZonelessChangeDetection } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { provideRouter } from '@angular/router';
 
 describe('App', () => {
+  let fixture: ComponentFixture<App>;
+  let compiled: HTMLElement;
+  let Component: App;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-      providers: [provideZonelessChangeDetection()]
+      providers: [provideZonelessChangeDetection(), provideRouter([])]
     }).compileComponents();
+    fixture = TestBed.createComponent(App);
+    compiled = fixture.nativeElement as HTMLElement;
+    Component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(App);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pokemon-ssr');
+  it(`should render the navbar and router-outlet`, () => {
+    expect(compiled.querySelector('app-navbar')).toBeTruthy();
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
+
 });
