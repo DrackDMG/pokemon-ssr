8dedd457eb32f6a8453d1f22ad909a16abab88bb
DrackDMG <drackmg@gmail.com>
2025-10-03 12:00:18 -0600

    test pokemon card

---
+++ src/app/pokemon/components/pokemon-card/pokemon-card.component.spec.ts
@@ -0,0 +1,54 @@
+import { provideZonelessChangeDetection } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PokemonCardComponent } from './pokemon-card.component';
+import { provideRouter } from '@angular/router';
+import { SimplePokemon } from '../../interfaces';
+
+describe('PokemonCardComponent', () => {
+  let fixture: ComponentFixture<PokemonCardComponent>;
+  let compiled: HTMLElement;
+  let component: PokemonCardComponent;
+
+  const mockPokemon: SimplePokemon = {
+    id: '1',
+    name: 'bulbasaur',
+  };
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PokemonCardComponent],
+      providers: [provideZonelessChangeDetection(), provideRouter([])]
+    }).compileComponents();
+    fixture = TestBed.createComponent(PokemonCardComponent);
+    fixture.componentRef.setInput('pokemon', mockPokemon);
+    compiled = fixture.nativeElement as HTMLElement;
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should have the SimplePokemon signal inputValue', () => {
+    expect(component.pokemon()).toEqual(mockPokemon);
+  });
+
+  it('should render the pokemon name and image correctly', () => {
+    const image = compiled.querySelector('img')!;
+    expect(image).toBeDefined();
+    const imageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${mockPokemon.id}.png`;
+    expect(image.src).toBe(imageUrl);
+    expect(compiled.textContent?.trim()).toBe(mockPokemon.name);
+  });
+
+  // it('should have the proper ng-reflect-router-link', () => {
+  //   console.log(compiled);
+  //   const divWithLink = compiled.querySelector('div');
+  //   console.log(divWithLink);
+  //   expect(
+  //     divWithLink?.attributes.getNamedItem('ng-reflect-router-link')?.value
+  //   ).toBe(`/pokemons,${mockPokemon.name}`);
+  // });
+
+});
